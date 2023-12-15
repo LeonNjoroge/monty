3fab7bfe5b89565fce175a90227ed46ea392e260
LeonNjoroge <leonmuraguri@gmail.com>
2023-12-15 17:58:08 +0300

    Task done

--- monty.c
+++ monty.c
@@ -1,54 +1,57 @@
 #include "monty.h"
-#include <stdio.h>
 
+#define MAX_LINE_LENGTH 1024
 
-/**
-  * main - The Monty Interpreter entry point
-  * @argn: The args number
-  * @args: The args passed to the interpreter
-  *
-  * Return: Always zero
-  */
+int main(int argn, char *args[]) {
+    FILE *fd = NULL;
+    size_t line_len = MAX_LINE_LENGTH;
+    unsigned int line_num = 1;
+    int op_status = 0;
+    char *filename = NULL, *op_code = NULL, *op_param = NULL, *buff = NULL;
 
-int main(int argn, char *args[])
-{
+    filename = args[1];
+    check_args_num(argn);
+    fd = open_file(filename);
 
-	FILE *fd = NULL;
-	size_t line_len = 0;
-	unsigned int line_num = 1;
-	int readed = 0, op_status = 0;
-	char *filename = NULL, *op_code = NULL, *op_param = NULL, *buff = NULL;
+    buff = (char *)malloc(line_len);
+    if (buff == NULL) {
+        perror("Error allocating memory");
+        fclose(fd);
+        return EXIT_FAILURE;
+    }
 
-	filename = args[1];
-	check_args_num(argn);
-	fd = open_file(filename);
+    while (fgets(buff, line_len, fd) != NULL) {
+        // Process the content of buff as needed
+        // Note: fgets includes the newline character in the string, so you may need to handle it
 
-	while ((readed = getline(&buff, &line_len, fd)) != -1)
-	{
-		op_code = strtok(buff, "\t\n ");
-		if (op_code)
-		{
-			if (op_code[0] == '#')
-			{
-				++line_num;
-				continue;
-			}
+        if (buff[strlen(buff) - 1] == '\n') {
+            // Remove the newline character if present
+            buff[strlen(buff) - 1] = '\0';
+        }
 
-			op_param = strtok(NULL, "\t\n ");
-			op_status = handle_execution(op_code, op_param, line_num, op_status);
+        op_code = strtok(buff, "\t\n ");
+        if (op_code) {
+            if (op_code[0] == '#') {
+                ++line_num;
+                continue;
+            }
 
-			if (op_status >= 100 && op_status < 300)
-			{
-				fclose(fd);
-				handle_error(op_status, op_code, line_num, buff);
-			}
-		}
+            op_param = strtok(NULL, "\t\n ");
+            op_status = handle_execution(op_code, op_param, line_num, op_status);
 
-		++line_num;
-	}
+            if (op_status >= 100 && op_status < 300) {
+                fclose(fd);
+                handle_error(op_status, op_code, line_num, buff);
+                free(buff);
+                return EXIT_FAILURE;
+            }
+        }
 
-	frees_stack();
-	free(buff);
-	fclose(fd);
-	return (0);
+        ++line_num;
+    }
+
+    frees_stack();
+    free(buff);
+    fclose(fd);
+    return EXIT_SUCCESS;
 }
